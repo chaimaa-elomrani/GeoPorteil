49e555c3318adf1d0529616b446520ac287489d6
chaimaa-elomrani <chaimaelomrani6@gmail.com>
2025-06-18 16:26:25 +0100

    added thelogin function

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const registerUser = async (req, res) => {
     try {
@@ -30,4 +31,39 @@ const registerUser = async (req, res) => {
     }
 };
 
-module.exports = {registerUser}; 
+
+
+const loginUser = async(req, res)=>{
+    try{
+        const {email, password} = req.body; 
+        const user = await User.findOne({email}); 
+
+        if(!user)
+            return res.status(400).json({message:"Email ou mot de passe incorrect"});
+
+        const PwdValidation = await bcrypt.compare(password, user.password); 
+
+        if(!PwdValidation)
+            return res.status(400).json({message:"Email ou mot de passe incorrect"});
+
+        // approved or not redirection will be here 
+
+        const token = jwt.sign(
+           {id:user._id , role: user.role}, 
+           process.env.JWT_EXPIRES, 
+           {expiresIn: process.env.JWT_EXPIRES || '2d'}
+        );
+
+        res.status(200).json({
+            message: "connexion réussie",
+            token, 
+            user: {id:user._id , email:user.email , role:user.role}
+
+        });
+    }catch(err){
+        console.error(err);
+        res.status(500).json({message: "Erreur serveur"})
+    }
+};
+
+module.exports = {registerUser, loginUser}; 
